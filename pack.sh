d8d333437e64cdf8949f39206eaeca494f42ece7
Daniella Mesquita <daniellmesquita@protonmail.com>
2022-02-10 17:10:29 -0300

    Improve pack.sh

--- pack.sh
+++ pack.sh
@@ -1,2 +1,11 @@
-tar -czvf central.tar.gz c2export
+#!/bin/bash
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+cp -r c2export central
+cd central
+ln -sf /usr/share/icons/Floflis/48x48@2x/apps/central.png icon.png
+cd "$SCRIPTPATH"
+tar -czvf central.tar.gz central
+rm -rf central
 echo "Saved in central.tar.gz"
