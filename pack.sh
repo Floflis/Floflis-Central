b6c76736ddd46ead10907588e9f7b2f9f1608e77
Daniella Mesquita <daniellmesquita@protonmail.com>
2022-02-12 13:13:23 -0300

    pack.sh: remove .git folder from export

--- pack.sh
+++ pack.sh
@@ -4,6 +4,7 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 cp -r c2export central
 cd central
+rm -rf .git
 ln -sf /usr/share/icons/Floflis/48x48@2x/apps/central.png icon.png
 cd "$SCRIPTPATH"
 tar -czvf central.tar.gz central
