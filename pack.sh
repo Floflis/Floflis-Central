d46e616b80e0ae2c61ec2d998f1060aa7c3aea57
Daniella Mesquita <daniellmesquita@protonmail.com>
2022-03-08 19:06:52 -0300

    pack.sh: rename variable app_id to application_id to reflect Floflis' disctinction of apps and games while they share the same "application" category

--- pack.sh
+++ pack.sh
@@ -2,14 +2,14 @@
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-app_id="central"
+application_id="central"
 
 mkdir application
-cp -r c2export application/$app_id
-cd "application/$app_id"
+cp -r c2export application/$application_id
+cd "application/$application_id"
 rm -rf .git
 cd "$SCRIPTPATH"
 #tar -czvf $app_id.apps application/$app_id
-tar -czvf $app_id.apps application
+tar -czvf $application_id.apps application
 rm -rf application
-echo "Saved in $app_id.apps"
+echo "Saved in $application_id.apps"
