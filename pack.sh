465c5fa18d093e9feccb58eb6a20a162661cba0e
Daniella Mesquita <daniellmesquita@protonmail.com>
2022-03-08 16:12:48 -0300

    Update pack.sh

--- pack.sh
+++ pack.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
@@ -7,6 +7,8 @@ cd central
 rm -rf .git
 ln -sf /usr/share/icons/Floflis/48x48@2x/apps/central.png icon.png
 cd "$SCRIPTPATH"
-tar -czvf central.tar.gz central
+#tar -czvf central.tar.gz central
+tar -czvf central.apps central
+#mv central.tar.gz central.apps
 rm -rf central
 echo "Saved in central.tar.gz"
